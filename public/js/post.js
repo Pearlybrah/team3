9688bdbc2060cb52ca1d0209c9d91209b904dc7a
Zachary Ferreira <Pearlybrah@github.com>
2019-01-22 15:55:28 -0800

    added window reload after submit function and is working

--- public/js/post.js
+++ public/js/post.js
@@ -27,34 +27,6 @@ $(document).ready(function() {
       }
     });
   };
-  //refresh the div -- in progress ---
-  function refreshDiv() {
-    app.get("/api", function(data) {
-      for (let i = 0; i < data.length; i++) {
-        var giftSection = $("<div>");
-        giftSection.attr("id", "gift-info-" + i);
-        $("#items").append(giftSection);
-
-        $("#gift-info-" + i).append("<p>Name: " + data[i].title + "</p>");
-        $("#gift-info-" + i).append(
-          "<p>Category: " + data[i].category + "</p>"
-        );
-        $("#gift-info-" + i).append(
-          "<p>Description: " + data[i].description + "</p>"
-        );
-        $("#gift-info-" + i).append(
-          "<p>Condition: " + data[i].condition + "</p>"
-        );
-        $("#gift-info-" + i).append(
-          "<p>Location: " + data[i].location + "</p>"
-        );
-        $("#gift-info-" + i).append("<img src=" + data[i].image + " />");
-      }
-    });
-  }
-
-  var refresh = $("#refresh");
-  refresh.addEventListener("click", refreshDiv, true);
 
   getGifts();
 });
