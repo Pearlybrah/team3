668936745b5aeec17599723b6fe71c85123e9735
RDNRYPPLSPRSN <rdnrypplsprsn@protonmail.com>
2019-01-22 18:49:17 -0800

    css improvements and item experimentation in post.js

--- public/js/post.js
+++ public/js/post.js
@@ -7,22 +7,11 @@ $(document).ready(function() {
         for (let i = 0; i < data.length; i++) {
           var giftSection = $("<div>");
           giftSection.attr("id", "gift-info-" + i);
+          giftSection.attr("class", "item-profile");
           $("#items").append(giftSection);
-
-          $("#gift-info-" + i).append("<p>Name: " + data[i].title + "</p>");
-          $("#gift-info-" + i).append(
-            "<p>Category: " + data[i].category + "</p>"
-          );
-          $("#gift-info-" + i).append(
-            "<p>Description: " + data[i].description + "</p>"
-          );
-          $("#gift-info-" + i).append(
-            "<p>Condition: " + data[i].condition + "</p>"
-          );
-          $("#gift-info-" + i).append(
-            "<p>Location: " + data[i].location + "</p>"
-          );
-          $("#gift-info-" + i).append("<img src=" + data[i].image + " />");
+          $("#gift-info-" + i).append("<span class='item-title'>" + data[i].title + "</span>");
+          $("#gift-info-" + i).append("<span class='item-location'>" + data[i].location + "</span>");
+          $("#gift-info-" + i).append("<img src=" + data[i].image + " + />");
         }
       }
     });
